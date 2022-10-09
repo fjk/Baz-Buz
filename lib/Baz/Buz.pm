9819042f01b7d41d98ba6c1d3b12bcdac14c59bb
fjk <frank.kropp@hotmail.com>
2022-10-09 03:21:21 +0200

    initial commit

---
+++ lib/Baz/Buz.pm
@@ -0,0 +1,32 @@
+package Baz::Buz;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Baz::Buz -
+
+=head1 SYNOPSIS
+
+  use Baz::Buz;
+
+=head1 DESCRIPTION
+
+Baz::Buz is
+
+=head1 AUTHOR
+
+fjk E<lt>fjkropp@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
